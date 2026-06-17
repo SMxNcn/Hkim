10e2f178141fa980195d87bb32040a836977c173
SMxNcn <liquidtube@outlook.com>
2026-06-17 18:04:35 +0800

    register missing :/

--- src/main/kotlin/cn/hkim/addon/Hkim.kt
+++ src/main/kotlin/cn/hkim/addon/Hkim.kt
@@ -41,7 +41,7 @@ object Hkim : ClientModInitializer {
         EventDispatcher.postEvents()
         EventDispatcher.registerListeners(DungeonUtils, EquipmentUtils, LocationUtils, TickTasks, WardrobeUtils)
         ModuleManager.registerAll(
-            Test, Animations, AutoLeap, AutoSprint, AutoSwap, CameraHelper, ClickGUI, CloseChest, CustomScoreboard, Etherwarp,
+            Test, Animations, AutoLeap, AutoSprint, AutoSwap, CameraHelper, CleanView, ClickGUI, CloseChest, CustomScoreboard, Etherwarp,
             FullBright, HurtCamera, ItemStar, LeapMenu, MainMenuModule, ModuleList, Nametags, RerollProtector, TitleManager
         )
         ModuleConfig.loadConfig()
