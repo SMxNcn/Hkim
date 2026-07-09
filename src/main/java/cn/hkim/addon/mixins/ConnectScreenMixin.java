8e229b5f78e343d4c195d05f0d6b5fa4446a63a1
SMxNcn <liquidtube@outlook.com>
2026-07-09 14:35:58 +0800

    feat(pause): replace player reporting button with server list button

--- src/main/java/cn/hkim/addon/mixins/ConnectScreenMixin.java
+++ src/main/java/cn/hkim/addon/mixins/ConnectScreenMixin.java
@@ -1,9 +1,13 @@
 package cn.hkim.addon.mixins;
 
+import cn.hkim.addon.mixins.accessors.JoinMultiplayerScreenAccessor;
 import cn.hkim.addon.utils.ServerUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ConnectScreen;
+import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.TransferState;
 import net.minecraft.client.multiplayer.resolver.ServerAddress;
@@ -19,6 +23,13 @@ public abstract class ConnectScreenMixin {
     private static void captureServerData(Screen parent, Minecraft minecraft, ServerAddress hostAndPort, ServerData data, boolean isQuickPlay, TransferState transferState, CallbackInfo ci) {
         if (data != null) {
             ServerUtils.setLastConnectionAttempt(data);
+        }
+
+        if (parent instanceof JoinMultiplayerScreen joinScreen) {
+            Screen lastScreen = ((JoinMultiplayerScreenAccessor) joinScreen).getLastScreen();
+            if (lastScreen instanceof PauseScreen && minecraft.level != null) {
+                minecraft.level.disconnect(ClientLevel.DEFAULT_QUIT_MESSAGE);
+            }
         }
     }
 }
