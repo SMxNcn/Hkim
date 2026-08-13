7fa06f903a6cdbce0c6f2c66926f635fa0e1ae0d
SMxNcn <liquidtube@outlook.com>
2026-08-13 23:48:49 +0800

    feat: add loadout command keybind
    - Let equipment wd command available

--- src/main/kotlin/cn/hkim/addon/features/impl/CommandKeybinds.kt
+++ src/main/kotlin/cn/hkim/addon/features/impl/CommandKeybinds.kt
@@ -6,16 +6,16 @@ import cn.hkim.addon.events.impl.InputEvent
 import cn.hkim.addon.features.Category
 import cn.hkim.addon.features.Module
 import cn.hkim.addon.features.ModuleInfo
-import cn.hkim.addon.utils.modMessage
 import cn.hkim.addon.utils.sendCommand
 import cn.hkim.addon.utils.skyblock.LocationUtils
 import meteordevelopment.orbit.EventHandler
 
 @ModuleInfo("command_keybinds", Category.SKYBLOCK)
-object CommandKeybinds : Module("Command Keybinds", "Various keybinds for common skyblock commands.") {
+object CommandKeybinds : Module("Command Keybinds", "Various keybinds for skyblock commands.") {
     private val pet by KeybindSetting("Pets", "Open the pets menu.")
     private val armorWd by KeybindSetting("Wardrobe", "Opens the wardrobe menu.")
     private val equipmentWd by KeybindSetting("Equipment Wardrobe", "Opens the equipment wardrobe menu.")
+    private val loadouts by KeybindSetting("Loadouts", "Opens the loadouts menu.")
     private val stats by KeybindSetting("Stats", "Open the stats menu. (Swap single equipment)")
     private val potion by KeybindSetting("Potion Bag", "Open the potion bag.")
     private val cookie by KeybindSetting("Cookie Menu", "Opens the cookie menu.")
@@ -26,10 +26,8 @@ object CommandKeybinds : Module("Command Keybinds", "Various keybinds for common
         when (event.key.value) {
             pet -> sendCommand("pets")
             armorWd -> sendCommand("wardrobe")
-            equipmentWd -> {
-                if (LocationUtils.inAlphaServer) sendCommand("equipment")
-                else modMessage("Equipment wardrobe not released yet!")
-            }
+            equipmentWd -> sendCommand("equipment")
+            loadouts -> sendCommand("loadouts")
             stats -> sendCommand("stats")
             potion -> sendCommand("potionbag")
             cookie -> sendCommand("boostercookie")
