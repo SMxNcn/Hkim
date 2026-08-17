688404a19c857c0f181733a0692f9bda34434156
SMxNcn <liquidtube@outlook.com>
2026-08-18 00:37:08 +0800

    feat(render): add Iris compat for CleanView

---
+++ src/main/java/cn/hkim/addon/mixins/CommonUniformsMixin.java
@@ -0,0 +1,23 @@
+package cn.hkim.addon.mixins;
+
+import cn.hkim.addon.features.impl.CleanView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Pseudo
+@Mixin(value = net.irisshaders.iris.uniforms.CommonUniforms.class)
+public class CommonUniformsMixin {
+
+    @Inject(method = "getBlindness", at = @At("HEAD"), cancellable = true)
+    private static void getBlindness(CallbackInfoReturnable<Float> cir) {
+        if (CleanView.shouldHideBlindness()) cir.setReturnValue(0f);
+    }
+
+    @Inject(method = "getDarknessFactor", at = @At("HEAD"), cancellable = true)
+    private static void getDarknessFactor(CallbackInfoReturnable<Float> cir) {
+        if (CleanView.shouldHideBlindness()) cir.setReturnValue(0f);
+    }
+}
